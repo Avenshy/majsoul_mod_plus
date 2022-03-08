b711ee629f4f93a603418429e39cc7f11d8bf127
Mushan Yang <mushanyoung@gmail.com>
2022-03-08 04:56:01 -0800

    Support github.com/quoid/userscripts (to extend this mod to Safari on iOS).
    quoid/userscripts has an different injection API than unsafeWindow.
    This change adds corresponding annotation and injects the MMP object
    into window.

--- 雀魂Mod_Plus.user.js
+++ 雀魂Mod_Plus.user.js
@@ -20,6 +20,8 @@
 // @match        https://mahjongsoul.game.yo-star.com/
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @inject-into  auto
+// @run-at       document-start
 // @license      GPL-3.0
 // ==/UserScript==
 
@@ -95,7 +97,14 @@ class MajsoulModPlus {
         }
     }
 }
-unsafeWindow.MMP = new MajsoulModPlus;
+
+MMP = new MajsoulModPlus;
+if (typeof unsafeWindow !== "undefined") {
+    unsafeWindow.MMP = MMP;
+} else {
+    console.log("unsafeWindow API not available. Try to inject into window.MMP");
+    window.MMP = MMP;
+}
 MMP.loadSettings();
 
 // 取称号id
@@ -1777,7 +1786,7 @@ function setAuto() {
                         // 保存人物和皮肤
                         MMP.settings.star_chars = uiscript.UI_Sushe.star_chars;
                         MMP.saveSettings();
-                        // END     
+                        // END
                     },
                     e.prototype.close = function (e) {
                         var i = this;
